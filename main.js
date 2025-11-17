970b1358ab1d13e784862349c0237214314c4be8
csd5124 <csd5124@csd.uoc.gr>
2025-11-18 01:18:57 +0200

    first

---
+++ main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFvz7MkFOd6rWFn0iti7PFCkFf2ZiYvBM",
+  authDomain: "mywebsite-88b73.firebaseapp.com",
+  projectId: "mywebsite-88b73",
+  storageBucket: "mywebsite-88b73.firebasestorage.app",
+  messagingSenderId: "227644999238",
+  appId: "1:227644999238:web:7988cf4921763ebd44ac47"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
